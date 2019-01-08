338aa58a25c7527ab22b78fa3dd9a0abc587b136
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-08 12:44:40 -0800

    built logic to find length of inputArray

--- data-structures-and-algorithms/Program.cs
+++ data-structures-and-algorithms/Program.cs
@@ -10,13 +10,40 @@ namespace data_structures_and_algorithms
         }
         static void ArrayShift()
         {
-
+            int[] shiftedArray = insertShiftArray(inputArray, newNum);
         }
 
-        static int[] insertShiftArray(int[] inputArray)
+        static int[] insertShiftArray(int[] inputArray, int newNum)
         {
-            int[] shiftedArray = new int[]; // length: determine length of inputArray and add 1
+            int[] shiftedArray = new int[findArrayLength(inputArray)];
+            // for loop to length - set shiftedArray[i] = inputArray[i] for left half, 
+            //                          shiftedArray[i] = newNum at middle, 
+            //                          shiftedArray[i+1] = inputArray[i] for right half
             return shiftedArray;
+        }
+
+        // finds length of inputArray and returns length of shiftedArray (1 more than inputArray)
+        static int findArrayLength(int[] inputArray)
+        {
+            int counter = 0;
+            int tester = 0;
+            bool endOfArray = false;
+            while (!endOfArray)
+            {
+                try
+                {
+                    tester = inputArray[counter]; // exception when ( counter = (last_index)+1 )
+                    counter++;
+                }
+                catch
+                {
+                    // counter = (last_index) + 1
+                    // inputArray_length = counter
+                    Console.Write($"inputArray contains ${counter} elements");
+                    counter++; // shiftedArray length is 1 longer than inputArray length, or (counter + 1)
+                }
+            }
+            return counter; // returns length of shiftedArray
         }
     }
 }
