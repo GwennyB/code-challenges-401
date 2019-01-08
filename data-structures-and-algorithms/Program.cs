95ac3e808aa3bf971be59116bfe2085e785e9cdd
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-08 12:25:06 -0800

    scaffolded per Canvas instructions

---
+++ data-structures-and-algorithms/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace data_structures_and_algorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayShift();
+        }
+        static void ArrayShift()
+        {
+
+        }
+
+        static int[] insertShiftArray(int[] inputArray)
+        {
+            int[] shiftedArray = new int[]; // length: determine length of inputArray and add 1
+            return shiftedArray;
+        }
+    }
+}
