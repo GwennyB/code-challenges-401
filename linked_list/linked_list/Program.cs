a7db2f63bf83ea1f6b1f8ba0d586e2bb2e3b9f74
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-14 18:59:40 -0800

    fixed Print to print entire list; fixed Print and Includes to handle empty lists

--- linked_list/linked_list/Program.cs
+++ linked_list/linked_list/Program.cs
@@ -8,13 +8,10 @@ namespace linked_list
         static void Main(string[] args)
         {
             LinkedList list = new LinkedList();
-            list.Insert(1);
-            list.Print(list.Head);
-            list.Insert(2);
-            list.Print(list.Head);
-            list.Insert(3);
-            list.Print(list.Head);
-            Console.ReadLine();
+            //list.Insert(1);
+            //list.Insert(2);
+            //list.Insert(3);
+            list.Print();
         }
     }
 }
