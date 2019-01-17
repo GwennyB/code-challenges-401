ce42511c14f2206828af7f03e992f498b15b591a
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-17 09:41:35 -0800

    application scaffolded; build helper method (BuildList) to generate random lists of specified size

---
+++ challenges/merge-lists/merge-lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using linked_list.Classes;
+
+namespace merge_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList mergedList = Merge(BuildList(4), BuildList(7));
+
+            mergedList.Print();
+
+            Console.ReadLine();
+        }
+
+        static LinkedList BuildList(int numNodes)
+        {
+            LinkedList list = new LinkedList();
+            Random rnd = new Random();
+            for (int i = 0; i < numNodes; i++)
+            {
+                list.Insert(rnd.Next(-100, 100));
+            }
+            return list;
+        }
+
+        static LinkedList Merge(LinkedList one, LinkedList two)
+        {
+
+
+        }
+
+
+    }
+}
