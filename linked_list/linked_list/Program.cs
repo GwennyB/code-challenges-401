37ce3236ead0883b5ad7c01c5ded61c41d9bc673
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-18 13:17:35 -0800

    updated InsertAfter to correct new node placement bug; updated tests to reflect proper test assertions

--- linked_list/linked_list/Program.cs
+++ linked_list/linked_list/Program.cs
@@ -12,35 +12,35 @@ namespace linked_list
 
         static void TestTheList()
         {
-            //LinkedList list = new LinkedList();
-            //list.Insert(1);
-            //list.Insert(2);
-            //list.Insert(3);
-            //list.InsertAfter(2, 999);
+            LinkedList list = new LinkedList();
+            list.Insert(8);
+            list.Insert(4);
+            list.Insert(5);
+            list.InsertAfter(5, 99);
             //list.InsertBefore(2, 888);
-            //list.Print();
+            list.Print();
             //testList.Append(88);
 
-            LinkedList testList = new LinkedList();
-            int[] testPoints = new int[] { 5, 4, 3, 2, 1 };
-            foreach (int point in testPoints)
-            {
-                testList.Insert(point);
-            }
+            //LinkedList testList = new LinkedList();
+            //int[] testPoints = new int[] { 5, 4, 3, 2, 1 };
+            //foreach (int point in testPoints)
+            //{
+            //    testList.Insert(point);
+            //}
 
-            testList.Print();
+            //testList.Print();
 
-            int kthFromEndValue = testList.FindFromEnd(2);
-            Console.WriteLine($"2 nodes from end: {kthFromEndValue}");
+            //int kthFromEndValue = testList.FindFromEnd(2);
+            //Console.WriteLine($"2 nodes from end: {kthFromEndValue}");
 
-            kthFromEndValue = testList.FindFromEnd(4);
-            Console.WriteLine($"4 nodes from end: {kthFromEndValue}");
+            //kthFromEndValue = testList.FindFromEnd(4);
+            //Console.WriteLine($"4 nodes from end: {kthFromEndValue}");
 
-            kthFromEndValue = testList.FindFromEnd(5);
-            Console.WriteLine($"5 nodes from end: {kthFromEndValue}");
+            //kthFromEndValue = testList.FindFromEnd(5);
+            //Console.WriteLine($"5 nodes from end: {kthFromEndValue}");
 
-            kthFromEndValue = testList.FindFromEnd(6);
-            Console.WriteLine($"6 nodes from end: {kthFromEndValue}");
+            //kthFromEndValue = testList.FindFromEnd(6);
+            //Console.WriteLine($"6 nodes from end: {kthFromEndValue}");
 
             Console.ReadLine();
         }
