c5fa9499012205be1ab445de01e182256225000d
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-01-30 11:03:56 -0800

    added imbalance-left and imbalance-right unit tests

--- breadth-first-traversal/UnitTests/UnitTest1.cs
+++ breadth-first-traversal/UnitTests/UnitTest1.cs
@@ -31,10 +31,10 @@ namespace UnitTests
         }
 
         /// <summary>
-        /// verifies that BreadthFirst returns values in correct order
+        /// verifies that BreadthFirst handles imbalance to left
         /// </summary>
         [Fact]
-        public void BreadthFirst_HandlesImbalancedTree()
+        public void BreadthFirst_HandlesImbalanceLeft()
         {
             BinaryTree tree = new BinaryTree();
             tree.Root = new Node(1);
@@ -46,6 +46,26 @@ namespace UnitTests
             tree.Root.Right.Right = new Node(7);
 
             int[] values = new int[] { 1, 2, 3, 4, /*5,*/ 6, 7 };
+
+            Assert.Equal(values, Program.BreadthFirst(tree));
+        }
+
+        /// <summary>
+        /// verifies that BreadthFirst handles imbalance to right
+        /// </summary>
+        [Fact]
+        public void BreadthFirst_HandlesImbalanceRight()
+        {
+            BinaryTree tree = new BinaryTree();
+            tree.Root = new Node(1);
+            tree.Root.Left = new Node(2);
+            tree.Root.Right = new Node(3);
+            tree.Root.Left.Left = new Node(4);
+            tree.Root.Left.Right = new Node(5);
+            //tree.Root.Right.Left = new Node(6);
+            tree.Root.Right.Right = new Node(7);
+
+            int[] values = new int[] { 1, 2, 3, 4, 5, /*6,*/ 7 };
 
             Assert.Equal(values, Program.BreadthFirst(tree));
         }
