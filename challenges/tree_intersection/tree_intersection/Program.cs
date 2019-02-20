d591cfc35f3b648ef970c048bba3478c5fb47835
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-19 21:31:09 -0800

    built method - testing required

--- challenges/tree_intersection/tree_intersection/Program.cs
+++ challenges/tree_intersection/tree_intersection/Program.cs
@@ -3,6 +3,7 @@ using StacksAndQueues.Classes;
 using tree.Classes;
 using Hashtable.Classes;
 using System.Collections.Generic;
+using linked_list.Classes;
 
 namespace tree_intersection
 {
@@ -13,6 +14,11 @@ namespace tree_intersection
             BinaryTree treeA = BuildBinTree(new int[] { 1,2,3,4,5,6,7 });
             BinaryTree treeB = BuildBinTree(new int[] { 1,-2,3,-4,5,-6,7 });
             List<object> list = TreeIntersection(treeA,treeB);
+            foreach (var item in list)
+            {
+                Console.WriteLine($"  {item}  ");
+            }
+            Console.ReadLine();
         }
 
         public static List<Object> TreeIntersection(BinaryTree treeA, BinaryTree treeB)
@@ -38,7 +44,7 @@ namespace tree_intersection
                 temp = queue.Dequeue();
                 if (!map.Contains(temp, null))
                 {
-
+                    map.Add(temp, null);
                 }
             }
 
@@ -46,8 +52,22 @@ namespace tree_intersection
             queue.Enqueue(treeB.Root);
             while (queue.Front != null)
             {
-
+                if (queue.Front.Left != null)
+                {
+                    queue.Enqueue(queue.Front.Left);
+                }
+                if (queue.Front.Right != null)
+                {
+                    queue.Enqueue(queue.Front.Right);
+                }
+                temp = queue.Dequeue();
+                if (!map.Contains(temp, null))
+                {
+                    map.Remove(temp, null);
+                    list.Add(temp);
+                }
             }
+            return list;
         }
 
 
@@ -63,13 +83,13 @@ namespace tree_intersection
             BinaryTree tree = new BinaryTree();
             if (values.Length > 0)
             {
-                tree.Root = new tree.Classes.Node(values[0]);
-                tree.Root.Left = new tree.Classes.Node(values[1]);
-                tree.Root.Right = new tree.Classes.Node(values[2]);
-                tree.Root.Left.Left = new tree.Classes.Node(values[3]);
-                tree.Root.Left.Right = new tree.Classes.Node(values[4]);
-                tree.Root.Right.Left = new tree.Classes.Node(values[5]);
-                tree.Root.Right.Right = new tree.Classes.Node(values[6]);
+                tree.Root = new Node(values[0]);
+                tree.Root.Left = new Node(values[1]);
+                tree.Root.Right = new Node(values[2]);
+                tree.Root.Left.Left = new Node(values[3]);
+                tree.Root.Left.Right = new Node(values[4]);
+                tree.Root.Right.Left = new Node(values[5]);
+                tree.Root.Right.Right = new Node(values[6]);
             }
             return tree;
         }
