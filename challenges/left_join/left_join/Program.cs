5ece909f69d4b5358b51ad842e9227b4b9b35a16
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-20 20:05:08 -0800

    added methoLeftJoin and PrintList methods

---
+++ challenges/left_join/left_join/Program.cs
@@ -0,0 +1,49 @@
+﻿using Hashtable.Classes;
+using linked_list.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace left_join
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static List<string> LeftJoin(Hashmap mapA, Hashmap mapB)
+        {
+            List<string> list = new List<string>();
+            string temp = "";
+            foreach (LinkedList item in mapA.Buckets)
+            {
+                item.Current = item.Head;
+                while(item.Current != null)
+                {
+                    temp = (string)(mapB.Get(item.Current.Key)) ?? "no antonym";
+                    temp = $"{(string)item.Current.Key}, {(string)item.Current.Value}, {temp}";
+                    list.Add(temp);
+                    temp = "";
+                    item.Current = item.Current.Next;
+                }
+            }
+            return list;
+        }
+
+        /// <summary>
+        /// TEST HELPER: prints values in list
+        /// </summary>
+        /// <param name="list"></param>
+        private static void PrintList(List<string> list)
+        {
+            Console.Write("List: ");
+            foreach (string item in list)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine("(end)");
+        }
+
+    }
+}
