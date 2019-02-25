97a9e4d84d5048c73385d4223b6ead71a0ada120
Gwendolyn Zubatch <gyzubatch@gmail.com>
2019-02-24 16:35:58 -0800

    scaffolded test suite

---
+++ StructuresAndAlgorithms/graph/UnitTests/UnitTest1.cs
@@ -0,0 +1,81 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Node can be successfully added to the graph
+        /// </summary>
+        [Fact]
+        public void CanAddNode()
+        {
+
+        }
+
+        /// <summary>
+        /// An edge can be successfully added to the graph
+        /// </summary>
+        [Fact]
+        public void CanAddEdge()
+        {
+
+        }
+
+        /// <summary>
+        /// A collection of all nodes can be properly retrieved from the graph
+        /// </summary>
+        [Fact]
+        public void CanReturnListOfAllNodes()
+        {
+
+        }
+
+        /// <summary>
+        /// All appropriate neighbors can be retrieved from the graph
+        /// </summary>
+        [Fact]
+        public void CanReturnListOfNeighbors()
+        {
+
+        }
+
+        /// <summary>
+        /// Neighbors are returned with the weight between nodes included
+        /// </summary>
+        [Fact]
+        public void CanReturnNeighborsWithWeights()
+        {
+
+        }
+
+        /// <summary>
+        /// The proper size is returned, representing the number of nodes in the graph
+        /// </summary>
+        [Fact]
+        public void CanReturnSize()
+        {
+
+        }
+
+        /// <summary>
+        /// A graph with only one node and edge can be properly returned
+        /// </summary>
+        [Fact]
+        public void CanReturnSingleNodeAndEdge()
+        {
+
+        }
+
+        /// <summary>
+        /// An empty graph properly returns null
+        /// </summary>
+        [Fact]
+        public void CanReturnNull()
+        {
+
+        }
+
+    }
+}
